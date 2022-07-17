346793a35707c0131c324f7a34b8791217e2c865
OpheliaXI <heliashelium@gmail.com>
2022-07-16 19:18:37 -0500

    Audit Ramuh

--- scripts/globals/abilities/pets/chaotic_strike.lua
+++ scripts/globals/abilities/pets/chaotic_strike.lua
@@ -14,8 +14,8 @@ end
 ability_object.onPetAbility = function(target, pet, skill)
     local numhits = 3
     local accmod = 1
-    local dmgmod = 9
-    local dmgmodsubsequent = 2
+    local dmgmod = 4
+    local dmgmodsubsequent = 4
     local totaldamage = 0
     local damage = xi.summon.avatarPhysicalMove(pet, target, skill, numhits, accmod, dmgmod, dmgmodsubsequent, xi.mobskills.magicalTpBonus.NO_EFFECT, 1, 2, 3)
     totaldamage = xi.summon.avatarFinalAdjustments(damage.dmg, pet, skill, target, xi.attackType.PHYSICAL, xi.damageType.BLUNT, numhits)
--- scripts/globals/abilities/pets/thunderstorm.lua
+++ scripts/globals/abilities/pets/thunderstorm.lua
@@ -27,8 +27,7 @@ ability_object.onPetAbility = function(target, pet, skill)
         tp = 300
     end
 
-    --note: this formula is only accurate for level 75 - 76+ may have a different intercept and/or slope
-    local damage = math.floor(512 + 1.72 * (tp + 1))
+    local damage = math.floor(96 * (((45/256) * tp) + (1370/256)))
     damage = damage + (dINT * 1.5)
     damage = xi.mobskills.mobMagicalMove(pet, target, skill, damage, xi.magic.ele.LIGHTNING, 1, xi.mobskills.magicalTpBonus.NO_EFFECT, 0)
     damage = xi.mobskills.mobAddBonuses(pet, target, damage.dmg, xi.magic.ele.LIGHTNING)
