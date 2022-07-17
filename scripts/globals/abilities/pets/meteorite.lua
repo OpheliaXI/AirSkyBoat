e7544a7279adf498a3a746d93d1c6ad1480c14f2
OpheliaXI <heliashelium@gmail.com>
2022-07-16 19:32:54 -0500

    Audit Leviathan | Audit Carbuncle

--- scripts/globals/abilities/pets/meteorite.lua
+++ scripts/globals/abilities/pets/meteorite.lua
@@ -14,8 +14,16 @@ end
 
 ability_object.onPetAbility = function(target, pet, skill)
     local dint = pet:getStat(xi.mod.INT) - target:getStat(xi.mod.INT)
-    local damage = 500 + dint*1.5 + skill:getTP()/20
+    local tp = skill:getTP()
+    local damage = 143
 
+    if tp < 1500 then
+        damage = math.floor(damage * (8/256) * (tp/10) + (896/256))
+    else
+        damage = math.floor(damage * ((8/256) * (1500/10)) + ((4/256) * ((tp-1500)/10) + 896/256))
+    end
+
+    damage = damage * (1.5 * dint)
     damage = xi.mobskills.mobMagicalMove(pet, target, skill, damage, xi.magic.ele.LIGHT, 1, xi.mobskills.magicalTpBonus.NO_EFFECT, 0)
     damage = xi.mobskills.mobAddBonuses(pet, target, damage.dmg, xi.magic.ele.LIGHT)
     damage = xi.summon.avatarFinalAdjustments(damage, pet, skill, target, xi.attackType.MAGICAL, xi.damageType.LIGHT, xi.mobskills.shadowBehavior.IGNORE_SHADOWS)
