7032fc6bbf408045f69e6650c252b6a36f52907c
OpheliaXI <heliashelium@gmail.com>
2022-12-03 17:56:05 -0600

    LSB to ASB Update 12/3

--- scripts/globals/abilities/call_beast.lua
+++ scripts/globals/abilities/call_beast.lua
@@ -52,6 +52,8 @@ abilityObject.onAbilityCheck = function(player, target, ability)
         if playerLevel < petLevels[petId] then return xi.msg.basic.NO_JUG_PET_ITEM, 0 end
         return 0, 0
     end
+
+    return 0, 0
 end
 
 abilityObject.onUseAbility = function(player, target, ability)
